b5c7ceaed504fc5662bbbbeb2b11a72fdc22a283
Nils Österling <nilsosterling@gmail.com>
2024-06-18 11:10:29 +0200

    Update config.java

--- src/main/java/config/config.java
+++ src/main/java/config/config.java
@@ -2,8 +2,8 @@ package config;
 
 public class config {
 	public static final String BASE_URL = "https://api.accounting.pe/v1";
-	public static final String API_KEY = "dHv8OF3vUMgdsSE"; //Test
-	public static final String CLIENT_ID = "3987"; //Test
+	public static final String API_KEY = "SETME"; //Test
+	public static final String CLIENT_ID = "SETME"; //Test
 	public static final int DAYS_BACK = 7;
 
 }
