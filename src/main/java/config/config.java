e325cbbdeb7468d1db8d8448138c1476ac7ea2cc
Nils Österling <nilsosterling@gmail.com>
2024-06-18 11:23:02 +0200

    gitignore

--- src/main/java/config/config.java
+++ src/main/java/config/config.java
@@ -3,7 +3,7 @@ package config;
 public class config {
 	public static final String BASE_URL = "https://api.accounting.pe/v1";
 	public static final String API_KEY = "SETME"; //Test
-	public static final String CLIENT_ID = "SETME"; //Test
+	public static final String CLIENT_ID = "SETSGSME"; //Test
 	public static final int DAYS_BACK = 7;
 
 }
